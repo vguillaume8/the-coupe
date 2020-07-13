d17971943cdbd803fc3be498da583d52342be607
Jabari Dash <jabari.dash@gmail.com>
2020-07-13 00:25:21 -0400

    almost done

--- README.md
+++ README.md
@@ -2,15 +2,9 @@
 website
 
 # todo
-tasks to complete
-
 - get a mission statement for welcome
 - center barber carousel
-- update captions on services or delete it
-- get names of cuts for the gallery
-- add more photos to gallery
-- replace background photo on about
-- look into getting reviews to replace testimony on about
 - update firebase file to redirect to index.html on 404
-- make social media links in foot open in new tab
 - add site meta tags
+- re-export black logo with transparent background
+- get favicon with black logo transparent background
--- public/js/main.js
+++ public/js/main.js
@@ -51,7 +51,7 @@
 
 	// loader
 	var loader = function() {
-		setTimeout(function() { 
+		setTimeout(function() {
 			if($('#ftco-loader').length > 0) {
 				$('#ftco-loader').removeClass('show');
 			}
@@ -166,19 +166,19 @@
 
 			if (st > 150) {
 				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
+					navbar.addClass('scrolled');
 				}
-			} 
+			}
 			if (st < 150) {
 				if ( navbar.hasClass('scrolled') ) {
 					navbar.removeClass('scrolled sleep');
 				}
-			} 
+			}
 			if ( st > 350 ) {
 				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
+					navbar.addClass('awake');
 				}
-				
+
 				if(sd.length > 0) {
 					sd.addClass('sleep');
 				}
@@ -218,7 +218,7 @@
 	};
 
 	var counter = function() {
-		
+
 		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-volunteer').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
@@ -235,7 +235,7 @@
 					  }, 7000
 					);
 				});
-				
+
 			}
 
 		} , { offset: '95%' } );
@@ -249,7 +249,7 @@
 		$('.ftco-animate').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -271,9 +271,9 @@
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '95%' } );
@@ -337,13 +337,4 @@
     fixedContentPos: false
   });
 
-  $('.appointment_date').datepicker({
-	  'format': 'm/d/yyyy',
-	  'autoclose': true
-	});
-
-	$('.appointment_time').timepicker();
-
-
 })(jQuery);
-
